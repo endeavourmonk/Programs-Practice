bd2e0ede450d06c2174b4db686faa30332695db8
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2022-04-19 12:00:29 +0530

    refactored code

--- C C++ PROGRAMS/c++ programs/factor2.cpp
+++ C C++ PROGRAMS/c++ programs/factor2.cpp
@@ -1,24 +1,17 @@
 // compute factors of a number..................
 
 #include <stdio.h>
-void compute_factor(int n)
-{
+void compute_factor(int n){
 	int d;
 	for(d=1; d<=n; d++)
-	{
 		if(n%d == 0)
-		{
 			  printf("%d ", d);
-		}
-	}
 }
 
-int main()
-{
+int main(){
 	int n;
 	printf("Enter a Number : ");
 	scanf("%d", &n);
-
 	compute_factor(n);
 	return 0;
 }
