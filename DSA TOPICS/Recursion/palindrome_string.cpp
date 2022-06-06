ddfbae38b789b3a12be171ffeb8133f1eea3a544
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2022-06-06 23:51:53 +0530

    added palindeome check

---
+++ DSA TOPICS/Recursion/palindrome_string.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string &str){
+    
+}
+
+int main(){
+    string str;
+    cin >> str;
+    bool flag = isPalindrome(str);
+    if(flag)    cout << "YES";
+    else    cout << "NO";
+    return 0;
+}
