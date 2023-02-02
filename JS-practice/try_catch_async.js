685bcf9758897bb00e7037eab4c94aaa563119f7
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-02-02 13:32:03 +0530

    added try catch code

---
+++ JS-practice/try_catch_async.js
@@ -0,0 +1,24 @@
+try {
+    console.log("Hello World!");
+} catch (error) {
+    console.log(error);
+}
+
+try {
+    console.log(Hello);
+} catch (error) {
+    console.log(error);
+}
+console.log("Done");
+try {
+    setTimeout(() => {
+        try {
+            console.log(Hello);
+        } catch (error) {
+            console.log(error, " :error inside setTimeout");
+        }
+    }, 1000);
+} catch (error) {
+    console.log(error);
+}
+console.log("done");
